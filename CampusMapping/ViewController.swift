12c7161495682a62dc2c48bd9982f2806c9ba9a0
Andrew Connor <andrewconnor2@gmail.com>
2022-03-11 17:14:22 +0000

    Initial commit

--- CampusMapping/ViewController.swift
+++ CampusMapping/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var mapView: MKMapView!
+    
 }
 
