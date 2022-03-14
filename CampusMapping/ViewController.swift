cc902fbce6fc15f6c3a4e1bfa066a5ed130fd676
Andrew Connor <andrewconnor2@gmail.com>
2022-03-14 00:20:54 +0000

    Status bar implemented along with constraints

--- CampusMapping/ViewController.swift
+++ CampusMapping/ViewController.swift
@@ -11,28 +11,41 @@ import MapKit
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
+
+    let CSDept = MapNode(latitude: 52.415904, longitude: -4.0650508, title: "Computer Science Building", subtitle: "Department of Computer Science")
     
     @IBOutlet weak var mapView: MKMapView!
-
+    
+    @IBOutlet weak var mapState: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let CSDept = MapNode(latitude: 52.415904, longitude: -4.0650508, title: "Computer Science Building", subtitle: "Department of Computer Science")
         // Sets a coordinate region with the Computer Science building as the centre and a 250m span.
-        let regionSpan: CLLocationDistance = 250
+        let regionSpan: CLLocationDistance = 500
         let coordinateRegion = MKCoordinateRegion(
              center: CSDept.coordinate,
              latitudinalMeters: regionSpan,
              longitudinalMeters: regionSpan)
        mapView.setRegion(coordinateRegion, animated: true)
        mapView.addAnnotation(CSDept)
+        
         // Asks for user location permission. 10m accuracy and shows the users location.
         locationManager.requestWhenInUseAuthorization()
         locationManager.distanceFilter = kCLDistanceFilterNone
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.startUpdatingLocation()
         mapView.showsUserLocation = true
+        
+        mapState.text = "State: Unknown"
     }
+    
+    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
 
+        // Do something with the location.
+        let csLocation = CLLocation(latitude: CSDept.coordinate.latitude, longitude: CSDept.coordinate.longitude)
+        let distance = csLocation.distance(from: manager.location!)
+        mapState.text = "Distance to Comp Sci is \(distance)"
+    }
 }
 
 class MapNode: NSObject, MKAnnotation {
