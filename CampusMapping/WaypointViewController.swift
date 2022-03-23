9d0573901de83bad770439f8b47ef3a298ddf40d
Andrew Connor <andrewconnor2@gmail.com>
2022-03-23 02:03:13 +0000

    Added placeholder string array to the table view

--- CampusMapping/WaypointViewController.swift
+++ CampusMapping/WaypointViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class WaypointViewController: UIViewController {
     
+    public var models: [String] = [
+        "Computer Science Building",
+        "University Reception",
+        "Hugh Owen Library"
+    ]
+    
     @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Waypoint"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -32,12 +40,13 @@ extension WaypointViewController: UITableViewDelegate {
 extension WaypointViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Computer Science Building"
+        cell.textLabel?.text = models[indexPath.row]
+//        cell.textLabel?.text = "Computer Science Building"
         return cell
     }
 }
